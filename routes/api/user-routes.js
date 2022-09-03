2f9e996aecc2b483c562f1081ded1880628d1055
dopecello <williamlocke.cello@gmail.com>
2022-09-03 14:55:54 -0400

    updated get routes for user to include votes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post } = require("../../models");
+const { User, Post, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
@@ -24,6 +24,12 @@ router.get("/:id", (req, res) => {
         model: Post,
         attributes: ["id", "title", "post_url", "created_at"],
       },
+      {
+        model: Postm
+        attributes: ["title"],
+        through: Vote,
+        as: 'voted_posts'
+      }
     ],
   })
     .then((dbUserData) => {
